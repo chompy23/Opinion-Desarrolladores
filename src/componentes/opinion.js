fe79f273e2b993b9d0470bde4d47afdb24feab68
chompy23 <sdefederico1@gmail.com>
2024-03-17 13:10:28 -0300

    Segundo commit es una prueba.

--- src/componentes/opinion.js
+++ src/componentes/opinion.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "../hojas de Estilo/opinion.css";
 
-
 function Opiniones(props) {
-    return (
-        <div className="contenedor-opiniones">
-            <img className="contenedor-imagenes"
-                src={require(`../imagenes/${props.imagen}.png`)}
+  return (
+    <div className="contenedor-opiniones">
+      <img
+        className="contenedor-imagenes"
+        src={require(`../imagenes/${props.imagen}.png`)}
                 alt={`"Foto de" ${props.nombre}`} />
             <div className="contenedor-texto-opiniones">
-                <p className="lugar-pais"><strong>{props.nombre}</strong> en {props.pais}</p>
-                <p className="puesto-developer">{props.cargo} en <strong>{props.empresa}</strong></p>
-                <p className="texto-comentario">"{props.opinion}"</p>
-            </div>
-        </div>
-    );
+        <p className="lugar-pais">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="puesto-developer">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-comentario">"{props.opinion}"</p>
+      </div>
+    </div>
+  );
 }
 export default Opiniones;
